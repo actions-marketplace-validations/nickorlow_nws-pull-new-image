2df58f2ce8693a400f671df194f7dadea12e5be7
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-07-25 00:31:11 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const applicationId = core.getInput('who-to-greet');
+  const deployKey = core.getInput('who-to-greet');
+  console.log(`ApplicationId ${applicationId}!`);
+  console.log(`DeployKey ${deployKey}!`);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
