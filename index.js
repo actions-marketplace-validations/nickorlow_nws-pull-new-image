b5127ca10a7154a4aa12111b906a0381d32dfe65
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-07-28 18:02:09 -0500

    added func

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 try {
+
   const applicationId = core.getInput('application-id');
   const deployKey = core.getInput('deploy-key');
   console.log(`ApplicationId ${applicationId}!`);
   console.log(`DeployKey ${deployKey}!`);
 
-  
+  axios
+  .put('https://api-nws.nickorlow.com', {"serviceId": serviceId, "deployKey": deployKey})
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
